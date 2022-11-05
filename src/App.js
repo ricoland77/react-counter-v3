3067153a9b4f41170742dcc7961e4d9c053c1ea9
mac-ricoland <ricoland@MacBook-Pro.local>
2022-11-05 10:47:52 +0100

    react-counter-v3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counters, setCounters] = useState([0]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {counters.length < 3 && (
+        <div className="top-add-counter">
+          <div>
+            <button
+              onClick={() => {
+                const newCounters = [...counters];
+                newCounters.push(0);
+                setCounters(newCounters);
+              }}
+            >
+              Add counter
+            </button>
+          </div>
+        </div>
+      )}
+
+      <div className="container">
+        {counters.map((counter, index) => {
+          return (
+            <div key={index} className="all-counters">
+              <div className="counter">
+                {counter !== 0 && (
+                  <button
+                    onClick={() => {
+                      const newCounters = [...counters];
+                      newCounters[index] -= 1;
+                      setCounters(newCounters);
+                    }}
+                  >
+                    -
+                  </button>
+                )}
+
+                <p>{counter}</p>
+                {counter < 10 && (
+                  <button
+                    onClick={() => {
+                      const newCounters = [...counters];
+                      newCounters[index] += 1;
+                      setCounters(newCounters);
+                    }}
+                  >
+                    +
+                  </button>
+                )}
+              </div>
+              <div>
+                <button
+                  onClick={() => {
+                    const newCounters = [...counters];
+                    newCounters[index] = 0;
+                    setCounters(newCounters);
+                  }}
+                >
+                  Reset
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
